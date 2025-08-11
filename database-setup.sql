4d4939b399dd76146984ab7e3819b29ee68cd2a2
Vaibhav Mehta <vaibhavvmehtavm@gmail.com>
2025-08-12 01:54:33 +0530

    Delete database-setup.sql

--- database-setup.sql
+++
@@ -1,9 +0,0 @@
--- Database setup for Bank Management System
--- Run this script in MySQL to create the database
-
--- Create database
-CREATE DATABASE IF NOT EXISTS bank_management_db;
-
--- Use the database
-USE bank_management_db;
-
